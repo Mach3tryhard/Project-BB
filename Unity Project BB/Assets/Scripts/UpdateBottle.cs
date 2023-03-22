0e21e350156af3141ecf79a149083a2948775d4d
Matei Sirghe <matei.sirghe@gmail.com>
2023-03-22 20:02:02 +0200

    0.04

--- Unity Project BB/Assets/Scripts/UpdateBottle.cs
+++ Unity Project BB/Assets/Scripts/UpdateBottle.cs
@@ -6,29 +6,29 @@ using TMPro;
 
 public class UpdateBottle : MonoBehaviour
 {
-    public GameObject BottleText1;
     public GameObject BoxText1;
+    public GameObject Letter;
+    public GameObject Canvas_forletter;
     private TMPro.TextMeshProUGUI _bobtext;
     private TMP_InputField _poptext;
     private void Start() 
     {
-        _bobtext=BottleText1.GetComponent<TMPro.TextMeshProUGUI>();
         _poptext=BoxText1.GetComponent<TMP_InputField>();
         _poptext.onValueChanged.AddListener (delegate {ValueChangeCheck ();});
     }
 
     public void ValueChangeCheck()
     {
-        if(_poptext.text.Length>25)
+        if(_poptext.text.Length>2)
         {
             string caca;
             caca=_poptext.text;
             caca=caca.Remove(0,1);
-            _bobtext.text=_bobtext.text+_poptext.text[0];
+            GameObject newletter;
+            newletter = Instantiate(Letter, new Vector3(0.0f,0.0f,-3.8f), Quaternion.identity,Canvas_forletter.transform);
+            newletter.SetActive(true);
+            newletter.transform.Find("TextLetter").GetComponent<TMPro.TextMeshProUGUI>().text=newletter.transform.Find("TextLetter").GetComponent<TMPro.TextMeshProUGUI>().text+_poptext.text[0];
             _poptext.text=caca;
-            //BottleText1.GetComponent<TMP_InputField>().text=_bobtext;
-            //BoxText1.GetComponent<TMP_InputField>().text=_poptext;
         }
     }
-
 }
