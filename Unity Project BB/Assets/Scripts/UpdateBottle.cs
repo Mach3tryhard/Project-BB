307802a9a7c3e5e172323743d06b91a3706f9c95
Matei Sirghe <matei.sirghe@gmail.com>
2023-03-23 22:00:11 +0200

    0.06

--- Unity Project BB/Assets/Scripts/UpdateBottle.cs
+++ Unity Project BB/Assets/Scripts/UpdateBottle.cs
@@ -6,9 +6,10 @@ using TMPro;
 
 public class UpdateBottle : MonoBehaviour
 {
+    int rez=0;
     public GameObject BoxText1;
     public GameObject Letter;
-    public GameObject Canvas_forletter;
+    public GameObject Parent_forletter;
     private TMPro.TextMeshProUGUI _bobtext;
     private TMP_InputField _poptext;
     private void Start() 
@@ -19,16 +20,36 @@ public class UpdateBottle : MonoBehaviour
 
     public void ValueChangeCheck()
     {
-        if(_poptext.text.Length>2)
+        if(_poptext.text.Length>20 && _poptext.text[0]!=' ')
         {
             string caca;
             caca=_poptext.text;
             caca=caca.Remove(0,1);
+            rez++;
             GameObject newletter;
-            newletter = Instantiate(Letter, new Vector3(0.0f,2.25f,-3.8f), Quaternion.identity,Canvas_forletter.transform);
+            newletter = Instantiate(Letter, new Vector3(0.0f,2.75f,-3.8f), Quaternion.identity,Parent_forletter.transform);
             newletter.SetActive(true);
             newletter.transform.Find("TextLetter").GetComponent<TMPro.TextMeshProUGUI>().text=""+_poptext.text[0];
             _poptext.text=caca;
+        }
+        else if(_poptext.text.Length>20)
+        {
+            string caca;
+            caca=_poptext.text;
+            caca=caca.Remove(0,1);
+            _poptext.text=caca;
+        }
+        
+    }
+
+    void Update()
+    {
+        if(rez==1250)
+        {
+            foreach (Transform child in Parent_forletter.transform)
+            {
+                GameObject.Destroy(child.gameObject);
+            }
         }
     }
 }
